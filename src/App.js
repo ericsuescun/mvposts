fd4ecb74b4f8a6911a8968f56abbded926fee6a5
Eric Suescun <edsuescun@gmail.com>
2019-12-23 18:58:30 -0500

    Se implementa con Redux en archivos distribuidos por funcion

--- src/App.js
+++ src/App.js
@@ -2,55 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
-// import store from './store';
+import store from './store';
 import posts from './posts';
-// import invrsSort from './sort';
-import { createStore } from 'redux';
-
-const adjust = (state, action) => {
-  let newItem;
-  let newList;
-  switch (action.type) {
-    case 'INCREMENT':
-      state.list.map((item, n) => {
-        if(n === action.index) {
-          newItem = { ...item, votes: item.votes + 1 };
-          newList = invrsSort([...state.list.slice(0, n), newItem, ...state.list.slice(n + 1)], state.order);  
-        }
-      });
-      return {...state, list: newList };
-    case 'DECREMENT':
-      state.list.map((item, n) => {
-        if(n === action.index) {
-          newItem = { ...item, votes: item.votes - 1 };
-          newList = invrsSort([...state.list.slice(0, n), newItem, ...state.list.slice(n + 1)], state.order);  
-        }
-      });
-      return {...state, list: newList };
-    case 'ORDER_ASC':
-        return {
-          order: false,
-          list: invrsSort(state.list, false)
-        };
-    case 'ORDER_DSC':
-        return {
-          order: true,
-          list: invrsSort(state.list, true)
-        };
-    default:
-        return state;
-  }
-}
-
-const invrsSort = (arry, polarity) => {
-  if(polarity) {
-    return arry.sort(function(a,b) { return b.votes - a.votes });
-  } else {
-    return arry.sort(function(a,b) { return a.votes - b.votes });  
-  }
-}
-
-const store = createStore(adjust, { order: true, list: invrsSort(posts, true) });
+import invrsSort from './sort';
 
 class App extends Component {
 
--- src/store.js
+++ src/store.js
@@ -3,41 +3,37 @@ import posts from './posts';
 import invrsSort from './sort';
 
 const adjust = (state, action) => {
+  let newItem;
+  let newList;
   switch (action.type) {
     case 'INCREMENT':
-    	console.log('+(' + action.index + ')');
-    	state.list.map((item, n) => {
-    		if(n === action.index) {
-    			
-    			// let newItem = { id: item.id, title: item.title, description: item.description, url: item.url, votes: item.votes + 1, writer_avatar_url: item.writer_avatar_url, post_image_url: item.post_image_url};
-    			let newItem = { ...item, votes: item.votes + 3 };
-    			let newList = invrsSort([...state.list.slice(0, n), newItem, ...state.list.slice(n + 1)], state.order);
-    			console.log(newList);
-    			console.log({...state, list: newList});
-    			return {
-    				...state,
-    				list: newList
-    			};
-    		} else {
-    			return state;
-    		}
-    	});
-    	return state;
+      state.list.map((item, n) => {
+        if(n === action.index) {
+          newItem = { ...item, votes: item.votes + 1 };
+          newList = invrsSort([...state.list.slice(0, n), newItem, ...state.list.slice(n + 1)], state.order);  
+        }
+      });
+      return {...state, list: newList };
     case 'DECREMENT':
-    	console.log('-');
-    	return state;
+      state.list.map((item, n) => {
+        if(n === action.index) {
+          newItem = { ...item, votes: item.votes - 1 };
+          newList = invrsSort([...state.list.slice(0, n), newItem, ...state.list.slice(n + 1)], state.order);  
+        }
+      });
+      return {...state, list: newList };
     case 'ORDER_ASC':
-      	return {
-      		order: false,
-      		list: invrsSort(state.list, false)
-      	};
+        return {
+          order: false,
+          list: invrsSort(state.list, false)
+        };
     case 'ORDER_DSC':
-      	return {
-      		order: true,
-      		list: invrsSort(state.list, true)
-      	};
+        return {
+          order: true,
+          list: invrsSort(state.list, true)
+        };
     default:
-      	return state;
+        return state;
   }
 }
 
