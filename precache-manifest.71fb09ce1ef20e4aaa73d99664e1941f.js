57a56198ff8ca2a7e50317822bf3fb0222dbd8e3
Eric Suescun <edsuescun@gmail.com>
2020-01-10 17:25:32 -0500

    Updates

---
+++ precache-manifest.71fb09ce1ef20e4aaa73d99664e1941f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec31c157a253a1964f1ffa3c417c51c2",
+    "url": "/mvposts/index.html"
+  },
+  {
+    "revision": "6272765a0299ef5ac9d0",
+    "url": "/mvposts/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "43a59277648da09c453d",
+    "url": "/mvposts/static/css/main.6043f99e.chunk.css"
+  },
+  {
+    "revision": "6272765a0299ef5ac9d0",
+    "url": "/mvposts/static/js/2.2a523124.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/mvposts/static/js/2.2a523124.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43a59277648da09c453d",
+    "url": "/mvposts/static/js/main.df3e17a5.chunk.js"
+  },
+  {
+    "revision": "e4c5f538218feb5d085f",
+    "url": "/mvposts/static/js/runtime-main.465a1196.js"
+  }
+]);
